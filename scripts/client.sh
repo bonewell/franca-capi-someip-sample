016170491072562f91e1c4d1f0049375753de05d
Konstantin Kolodiy <KKolodiy@luxoft.com>
2016-11-01 05:34:55 -0700

    Configure for using UDP
    - And fix scripts to run

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export VSOMEIP_APPLICATION_NAME=client-sample
-export VSOMEIP_CONFIGURATION=~/git/franca-capi-someip-sample/fidl/vsomeipd-client.json
+export VSOMEIP_CONFIGURATION=~/git/franca-capi-someip-sample/fidl/vsomeip-client.json
 ~/git/franca-capi-someip-sample/build/HelloWorldClient
--- scripts/service.sh
+++ scripts/service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export VSOMEIP_APPLICATION_NAME=service-sample
-export VSOMEIP_CONFIGURATION=~/git/franca-capi-someip-sample/fidl/vsomeipd-service.json
+export VSOMEIP_CONFIGURATION=~/git/franca-capi-someip-sample/fidl/vsomeip-service.json
 ~/git/franca-capi-someip-sample/build/HelloWorldService
