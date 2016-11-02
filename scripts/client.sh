afc69b005d5170a2c913d38c1ddbcd6e7a6dde44
Konstantin Kolodiy <KKolodiy@luxoft.com>
2016-11-02 15:59:48 +0200

    Improve building and installing

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export VSOMEIP_APPLICATION_NAME=client-sample
-export VSOMEIP_CONFIGURATION=~/git/franca-capi-someip-sample/fidl/vsomeip-client.json
-~/git/franca-capi-someip-sample/build/HelloWorldClient
+export VSOMEIP_CONFIGURATION=..config/vsomeip-client.json
+../bin/client
--- scripts/service.sh
+++ scripts/service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export VSOMEIP_APPLICATION_NAME=service-sample
-export VSOMEIP_CONFIGURATION=~/git/franca-capi-someip-sample/fidl/vsomeip-service.json
-~/git/franca-capi-someip-sample/build/HelloWorldService
+export VSOMEIP_CONFIGURATION=../config/vsomeip-service.json
+../bin/service
