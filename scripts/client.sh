345271e16fad5adff9548665c4e7ad1ba0c55768
Konstantin Kolodiy <KKolodiy@luxoft.com>
2017-01-27 11:12:40 +0200

    Fix bash scripts

--- scripts/client.sh
+++ scripts/client.sh
@@ -3,7 +3,8 @@
 sudo route add -net 224.0.0.0/4 dev ens33
 COMMONAPI_PATH=~/git/genivi/capicxx-core-runtime/build
 COMMONAPI_SOMEIP_PATH=~/git/genivi/capicxx-someip-runtime/build
-export LD_LIBRARY_PATH=${COMMONAPI_PATH}:${COMMONAPI_SOMEIP_PATH}:$LD_LIBRARY_PATH
+VSOMEIP_PATH=~/git/genivi/vsomeip/build
+export LD_LIBRARY_PATH=../lib:${COMMONAPI_PATH}:${COMMONAPI_SOMEIP_PATH}:${VSOMEIP_PATH}:$LD_LIBRARY_PATH
 export VSOMEIP_APPLICATION_NAME=client-sample
 export VSOMEIP_CONFIGURATION=../config/vsomeip-client.json
 export COMMONAPI_CONFIG=../config/commonapi.ini
--- scripts/service.sh
+++ scripts/service.sh
@@ -2,8 +2,9 @@
 
 
 COMMONAPI_PATH=~/git/genivi/capicxx-core-runtime/build
-COMMONAPI_SOMEIP=~/git/genivi/capicxx-someip-runtime/build
-export LD_LIBRARY_PATH=${COMMONAPI_PATH}:${COMMONAPI_SOMEIP_PATH}:$LD_LIBRARY_PATH
+COMMONAPI_SOMEIP_PATH=~/git/genivi/capicxx-someip-runtime/build
+VSOMEIP_PATH=~/git/genivi/vsomeip/build
+export LD_LIBRARY_PATH=../lib:${COMMONAPI_PATH}:${COMMONAPI_SOMEIP_PATH}:${VSOMEIP_PATH}:$LD_LIBRARY_PATH
 export VSOMEIP_APPLICATION_NAME=service-sample
 export VSOMEIP_CONFIGURATION=../config/vsomeip-service.json
 export COMMONAPI_CONFIG=../config/commonapi.ini
