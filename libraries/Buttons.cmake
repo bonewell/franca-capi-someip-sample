9031074f299d615cab5aae37f0ae21ffd0609c49
Konstantin Kolodiy <KKolodiy@luxoft.com>
2017-02-02 17:35:13 +0200

    Change macros and function for CMake

---
+++ libraries/Buttons.cmake
@@ -0,0 +1,3 @@
+add_someip_interfaces(Buttons com.ford.hmi.Buttons:1)
+add_someip_library(Buttons)
+
--- libraries/ML.cmake
+++ libraries/ML.cmake
@@ -1,4 +1,4 @@
-add_someip_types(ford.rnd.MLTypes:1)
-add_someip_interfaces(ford.rnd.ML:1 ford.rnd.SmartML:1)
+add_someip_types(ML ford.rnd.MLTypes:1)
+add_someip_interfaces(ML ford.rnd.ML:1 ford.rnd.SmartML:1)
 add_someip_library(ML)
 
